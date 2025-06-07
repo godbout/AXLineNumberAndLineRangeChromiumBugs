1374168606f8f081cd5c19d6ca40f285bd9aaec9
Guillaume Leclerc <guill@sleeplessmind.com.mo>
2025-06-07 16:04:07 +0800

    first commit

---
+++ AXLineNumberAndLineRangeChromiumBugs/AXLineNumberAndLineRangeChromiumBugsApp.swift
@@ -0,0 +1,17 @@
+//
+//  AXLineNumberAndLineRangeChromiumBugsApp.swift
+//  AXLineNumberAndLineRangeChromiumBugs
+//
+//  Created by Guillaume Leclerc on 07/06/2025.
+//
+
+import SwiftUI
+
+@main
+struct AXLineNumberAndLineRangeChromiumBugsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
