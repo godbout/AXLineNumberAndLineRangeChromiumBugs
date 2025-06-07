c7d5d5831caca4522aff99337f07cd71d1dcffd0
Guillaume Leclerc <guill@sleeplessmind.com.mo>
2025-06-07 16:07:31 +0800

    add code lol

--- AXLineNumberAndLineRangeChromiumBugs/AXLineNumberAndLineRangeChromiumBugsApp.swift
+++ AXLineNumberAndLineRangeChromiumBugs/AXLineNumberAndLineRangeChromiumBugsApp.swift
@@ -9,9 +9,16 @@ import SwiftUI
 
 @main
 struct AXLineNumberAndLineRangeChromiumBugsApp: App {
+    init() {
+        let options: NSDictionary = [kAXTrustedCheckOptionPrompt.takeRetainedValue() as NSString: true]
+        _ = AXIsProcessTrustedWithOptions(options)
+    }
+
     var body: some Scene {
         WindowGroup {
             ContentView()
         }
+        .defaultSize(width: 288, height: 188)
+        .defaultPosition(.topLeading)
     }
 }
--- AXLineNumberAndLineRangeChromiumBugs/ContentView.swift
+++ AXLineNumberAndLineRangeChromiumBugs/ContentView.swift
@@ -1,22 +1,79 @@
 //
 //  ContentView.swift
-//  AXLineNumberAndLineRangeChromiumBugs
+//  AXRangeForLineElectronBug
 //
-//  Created by Guillaume Leclerc on 07/06/2025.
+//  Created by Guillaume Leclerc on 06/06/2025.
 //
 
 import SwiftUI
 
 struct ContentView: View {
+    @State private var caretLocation: Int?
+    @State private var lineNumber: Int?
+    @State private var lineStartLocation: Int?
+    @State private var lineLength: Int?
+    
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+            Button("get AX info") {
+                sleep(3)
+
+                let axSystemWideElement = AXUIElementCreateSystemWide()
+                    
+                var axFocusedElement: AnyObject?
+                guard AXUIElementCopyAttributeValue(axSystemWideElement, kAXFocusedUIElementAttribute as CFString, &axFocusedElement) == .success else {
+                    print("can't get system wide element")
+                        
+                    return
+                }
+
+                var axSelectedTextRange: AnyObject?
+                guard AXUIElementCopyAttributeValue(axFocusedElement as! AXUIElement, kAXSelectedTextRangeAttribute as CFString, &axSelectedTextRange) == .success else {
+                    print("can't get text range")
+                        
+                    return
+                }
+                    
+                var selectedTextRange = CFRange()
+                AXValueGetValue(axSelectedTextRange as! AXValue, .cfRange, &selectedTextRange)
+                caretLocation = selectedTextRange.location
+                    
+                var currentLine: AnyObject?
+                guard AXUIElementCopyParameterizedAttributeValue(axFocusedElement as! AXUIElement, kAXLineForIndexParameterizedAttribute as CFString, selectedTextRange.location as CFTypeRef, &currentLine) == .success else {
+                    print("can't get current line")
+                        
+                    return
+                }
+                lineNumber = currentLine as? Int
+
+                var lineRangeValue: AnyObject?
+                guard AXUIElementCopyParameterizedAttributeValue(axFocusedElement as! AXUIElement, kAXRangeForLineParameterizedAttribute as CFString, currentLine as CFTypeRef, &lineRangeValue) == .success else {
+                    print("can't get line range")
+                        
+                    return
+                }
+                    
+                var lineRange = CFRange()
+                AXValueGetValue(lineRangeValue as! AXValue, .cfRange, &lineRange)
+                lineStartLocation = lineRange.location
+                lineLength = lineRange.length
+            }
+
+            Spacer()
+            
+            VStack(alignment: .leading) {
+                Text("caret location (0 based): \(byeByeOptionalInStrings(for: caretLocation))")
+                Text("line number (0 based): \(byeByeOptionalInStrings(for: lineNumber))")
+                Text("line start location (0 based): \(byeByeOptionalInStrings(for: lineStartLocation))")
+                Text("line length: \(byeByeOptionalInStrings(for: lineLength))")
+            }
         }
         .padding()
     }
+}
+
+func byeByeOptionalInStrings(for int: Int?) -> String {
+    int == nil ? "nil" : String(int!)
 }
 
 #Preview {
